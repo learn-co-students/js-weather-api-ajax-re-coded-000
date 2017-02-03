39582841f216031b8c05d0ad66d8136f8b93e690
Ali Alrawi <ali.developer90@gmail.com>
2017-02-03 13:13:02 +0300

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -35,8 +35,35 @@ function generateDataSet(labels, data) {
         }
     ]
 };
-
 return data_
+// the object below is working prety fine but the test needs something else ..
+//   var data_ = {
+//     labels: labels,
+//     datasets: [
+//         {
+//             label: "Hourly Weather for New York",
+//             fill: false,
+//             lineTension: 0.1,
+//             backgroundColor: "rgba(75,192,192,0.4)",
+//             borderColor: "rgba(75,192,192,1)",
+//             borderCapStyle: 'butt',
+//             borderDash: [],
+//             borderDashOffset: 0.0,
+//             borderJoinStyle: 'miter',
+//             pointBorderColor: "rgba(75,192,192,1)",
+//             pointBackgroundColor: "#fff",
+//             pointBorderWidth: 1,
+//             pointHoverRadius: 5,
+//             pointHoverBackgroundColor: "rgba(75,192,192,1)",
+//             pointHoverBorderColor: "rgba(220,220,220,1)",
+//             pointHoverBorderWidth: 2,
+//             pointRadius: 1,
+//             pointHitRadius: 10,
+//             data: data,
+//             spanGaps: false,
+//         }
+//     ]
+// };
 }
 
 

11e4a1459b9d2d44299756d9da4e1e554f5d92bd
Ali Alrawi <ali.developer90@gmail.com>
2017-02-03 12:14:32 +0300

    checkpoint 1

--- js/init.js
+++ js/init.js
@@ -1,10 +1,16 @@
+
 jQuery(document).ready(function($) {
-  var API_KEY = "GO GET AN API KEY";
+  var API_KEY = "edd1872e01f81ec7";
   var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
   var ctx = $("#NYCWeatherChart").get(0).getContext("2d");
 
   makeAjaxRequest(URL, function(json) {
+  	
     var data = generateDataSet(getHours(json), getFarenheits(json));
-    var tempChart = new Chart.Line(ctx, {data: data, options: { bezierCurve: true}});
+    //   console.log(data);
+    // new Chart(ctx).Line(data);
+   var tempChart = new Chart.Line(ctx, {data: data, options: { bezierCurve: true}});
+
   });
 });
+//   http://api.wunderground.com/api/edd1872e01f81ec7/hourly/q/NY/New_York.json
--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,59 @@
+
 function getFarenheits(result){
   // Your code goes here
+   var hours=[] ;
+  //console.log(result.hourly_forecast)
+  $.each(result.hourly_forecast,function(i,obj){
+     hours.push(obj.FCTTIME.hour) ;
+  });
+  return hours;
 }
 
 function getHours(result){
   // Your code goes here
+  var temps=[] ;
+  $.each(result.hourly_forecast,function(i,obj){
+     temps.push(obj.temp.english) ;
+  });
+  return temps;
 }
 
 function generateDataSet(labels, data) {
   // Your code goes here
+  var data_ = {
+    labels: labels,
+    datasets: [
+        {
+            label: "My First dataset",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: data,
+            spanGaps: false,
+        }
+    ]
+};
+return data_
 }
 
 function makeAjaxRequest(endpoint, success) {
   // Your code goes here
+   // $.ajax({url: endpoint,success: function(response) { success(response); }});
+     $.ajax({ url : endpoint, dataType : "jsonp", success: success})
 }
+
+

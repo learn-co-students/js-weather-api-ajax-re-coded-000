c0a8e54d2ae148353c13cdbbfcf116811fda2720
Ekhlas Rain <baran.rain1994@gmail.com>
2017-03-12 23:50:08 +0300

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,37 @@
 function getFarenheits(result){
+  return result.hourly_forecast.map(function(itm){
+  	return itm.temp.english
+  })
   // Your code goes here
 }
 
 function getHours(result){
+	return result.hourly_forecast.map(function(itm){
+		return itm.FCTTIME.hour
+	})
   // Your code goes here
 }
 
 function generateDataSet(labels, data) {
+	return {
+    labels: labels,
+    datasets: [
+    {
+      label: "Hourly Weather for New York",
+      fillColor: "rgba(220,220,220,0.2)",
+      strokeColor: "rgba(220,220,220,1)",
+      pointColor: "rgba(220,220,220,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(220,220,220,1)",
+      data: data
+    }
+    ]
+  };
   // Your code goes here
 }
 
 function makeAjaxRequest(endpoint, success) {
+	$.ajax({ url : endpoint, dataType : "jsonp", success: success})
   // Your code goes here
 }

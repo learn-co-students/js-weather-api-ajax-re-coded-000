041d3056311dfcc6a2ece3af319d5294d0776c43
dlkhwaz <dllaomer93@gmail.com>
2017-03-13 16:20:17 +0300

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,44 @@
-function getFarenheits(result){
-  // Your code goes here
+function getFarenheits(json){
+	return $.map(json.hourly_forecast, function(itm){
+		return itm.temp.english
+
+	})
 }
 
-function getHours(result){
-  // Your code goes here
+function getHours(json){
+	return $.map(json.hourly_forecast, function(itm){
+		return itm.FCTTIME.hour
+
+	})
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+	return {
+    labels: labels,
+    datasets: [
+        {
+        label: "Hourly Weather for New York",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: data,
+        }
+     
+    ]
+};
+	// var myLineChart= new Chart(ctx).Line(data);
+	
 }
 
 function makeAjaxRequest(endpoint, success) {
+	$.ajax({
+		url: endpoint,
+		dataType: "jsonp",
+		success: success
+	})
+
   // Your code goes here
 }

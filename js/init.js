18596934c93c31a52b453e0da48f64e92dc2efc0
dina aziz <dina.aziz100@yahoo.com>
2017-03-13 09:16:33 +0000

    Done.

--- js/init.js
+++ js/init.js
@@ -1,10 +1,10 @@
-jQuery(document).ready(function($) {
-  var API_KEY = "GO GET AN API KEY";
-  var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
+
+$(document).ready(function($) {
+  var API_KEY = 'b8bd4abc5f3ddab1';
+  var URL = "http://api.wunderground.com/api/" + 'b8bd4abc5f3ddab1' + "/hourly/q/NY/New_York.json";
   var ctx = $("#NYCWeatherChart").get(0).getContext("2d");
 
   makeAjaxRequest(URL, function(json) {
     var data = generateDataSet(getHours(json), getFarenheits(json));
-    var tempChart = new Chart.Line(ctx, {data: data, options: { bezierCurve: true}});
-  });
+new Chart(ctx).Line(data);  });
 });
